7e97e8c37b51dbd02782611405e0177255d4ab47
PrakharGoyal28 <PRAKHAR28GOYAL@GMAIL.COM>
2024-07-13 11:56:16 +0530

    login and register

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,25 +1,19 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css'
-import Login from './login'
-import { SignUpFour } from './register'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './login';
+import Register from './register';
 
-function App() {
-
+const App = () => {
   return (
-    
-      <div className='text-white h-[100vh] flex items-center justify-center bg-cover'>
-        <Router>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route path="/register" component={SignUpFour} />
-      </Switch>
-    </Router>
-          
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </div>
-      
-    
-  )
-}
+    </Router>
+  );
+};
 
-export default App
+export default App;
